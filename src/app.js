8d2463405ded37d663d854d340fb74065a8bb870
Anna Tasheva <anna.tasheva90@gmail.com>
2024-08-06 11:15:27 +0100

    Created a POST, PUT and DELETE requests and tested them in Thunder Client

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const app = express();
 const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
-//TODO: Create your GET Request Route Below:
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/restaurants", async (req, res) => {
   const allRestaurants = await Restaurant.findAll();
@@ -17,6 +18,28 @@ app.get("/restaurants/:id", async (req, res) => {
   const currentRestaurant = await Restaurant.findByPk(restaurantId);
 
   res.json(currentRestaurant);
+});
+
+app.post("/restaurants", async (req, res) => {
+  const createNewRestaurant = await Restaurant.create(req.body);
+  res.json(createNewRestaurant);
+});
+
+app.put("/restaurants/:id", async (req, res) => {
+  let restaurantId = req.params.id;
+  let updateObject = req.body;
+  const updatedRestaurant = await Restaurant.update(updateObject, {
+    where: { id: restaurantId },
+  });
+  res.json(updatedRestaurant);
+});
+
+app.delete("/restaurants/:id", async (req, res) => {
+  let restaurantId = req.params.id;
+  const deletedRestaurant = await Restaurant.destroy({
+    where: { id: restaurantId },
+  });
+  res.json(deletedRestaurant);
 });
 
 module.exports = app;
